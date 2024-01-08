f81a7e078478384064602a7adabb6427d03a739e
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-01-07 23:02:48 -0800

    Add files via upload

---
+++ Kilometers to Miles_prob_8.py
@@ -0,0 +1,6 @@
+kilometers = int (input("Enter  value in  kilometer: "))
+conv_fact = 0.621371
+miles = kilometers*conv_fact
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
+
+
